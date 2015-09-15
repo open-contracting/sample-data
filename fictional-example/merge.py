14d8435c5cc2e0d9e698fa00e1f1a03484d16755
Ben Webb <bjwebb67@googlemail.com>
2015-09-15 17:13:16 +0100

    Tidy up merge.py
    Former-commit-id: 7fc2ffeb310e25879a09afb86b477b48942b4dbb

--- fictional-example/merge.py
+++ fictional-example/merge.py
@@ -1,29 +1,26 @@
 import os
 import jsonmerge
 import json
-from jsonschema import validate
-from collections import defaultdict
-from operator import itemgetter
-from copy import deepcopy
 
 with open('../../standard/standard/schema/release-schema.json', 'r') as f:
-    release_schema = json.loads(f.read())
+    release_schema = json.load(f)
 
 contacting_processes = {}
 
 # Get all the JSON files in this directory
-for f in os.listdir("."):
-    if f.endswith('json'):
-        with open(f,'r') as jsonfile:
-            package = json.loads(jsonfile.read())
+for fname in os.listdir("."):
+    if fname.endswith('json'):
+        print(fname)
+        with open(fname, 'r') as jsonfile:
+            package = json.load(jsonfile)
             for release in package['releases']:
                 try:
                     base
-                except:
+                except NameError:
                     base = release
-                    
-                base = jsonmerge.merge(base, release) #  ,release_schema
+                    continue  # Don't merge the first release onto itself
 
+                base = jsonmerge.merge(base, release)  # ,release_schema
 
 with open("record/record.json", 'w') as f:
-    f.write(json.dumps(base,indent=3,sort_keys=True))
+    json.dump(base, f, indent=3, sort_keys=True)
