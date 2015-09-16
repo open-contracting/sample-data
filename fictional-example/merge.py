f820c34491269a1f010d20c2c3ae3b390ec95549
Ben Webb <bjwebb67@googlemail.com>
2015-09-16 14:44:13 +0100

    To merge using schema, base should be a versionedRelease
    We can start the process with base being an empty dict.
    
    
    Former-commit-id: a8b2f4e3978daf9840744ecf2f53034dc415e57d

--- fictional-example/merge.py
+++ fictional-example/merge.py
@@ -5,7 +5,7 @@ import json
 with open('../../standard/standard/schema/release-schema.json', 'r') as f:
     release_schema = json.load(f)
 
-contacting_processes = {}
+base = {}
 
 # Get all the JSON files in this directory
 for fname in sorted(os.listdir(".")):
@@ -14,13 +14,7 @@ for fname in sorted(os.listdir(".")):
         with open(fname, 'r') as jsonfile:
             package = json.load(jsonfile)
             for release in package['releases']:
-                try:
-                    base
-                except NameError:
-                    base = release
-                    continue  # Don't merge the first release onto itself
-
-                base = jsonmerge.merge(base, release)  # ,release_schema
+                base = jsonmerge.merge(base, release, release_schema)
 
 with open("record/record.json", 'w') as f:
     json.dump(base, f, indent=3, sort_keys=True)
