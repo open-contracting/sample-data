fd8b5a50787d7b9856b55bfa0605a6bfb3313ca1
Ben Webb <bjwebb67@googlemail.com>
2015-09-16 15:23:10 +0100

    Create a record package, with record containing compiled and versioned
    Former-commit-id: 285ec6da24b9228654742c1d527910ad9fdc34bb

--- fictional-example/merge.py
+++ fictional-example/merge.py
@@ -5,16 +5,35 @@ import json
 with open('../../standard/standard/schema/release-schema.json', 'r') as f:
     release_schema = json.load(f)
 
-base = {}
+compiled_release = {}
+versioned_release = {}
+releases = []
+package = None
 
 # Get all the JSON files in this directory
 for fname in sorted(os.listdir(".")):
     if fname.endswith('json'):
-        print(fname)
         with open(fname, 'r') as jsonfile:
             package = json.load(jsonfile)
             for release in package['releases']:
-                base = jsonmerge.merge(base, release, release_schema)
+                releases.append({
+                    'url': package['uri'] + '#' + release['id'],
+                    'date': release['date'],
+                    'tag': release['tag']
+                })
+                versioned_release = jsonmerge.merge(versioned_release, release, release_schema)
+                compiled_release = jsonmerge.merge(compiled_release, release)
 
-with open("record/record.json", 'w') as f:
-    json.dump(base, f, indent=3, sort_keys=True)
+with open("record/ocds-213czf-000-00001.json", 'w') as f:
+    json.dump({
+        'uri': 'http://standard.open-contracting.org/examples/records/ocds-213czf-000-00001.json',
+        'packages': [package['uri']],
+        'publisher': package['publisher'],
+        'publishedDate': '2009-03-15',
+        'records': [{
+            'ocid': 'ocds-213czf-000-00001',
+            'releases': releases,
+            'compiledRelease': compiled_release,
+            'versionedRelease': versioned_release,
+        }]
+    }, f, indent=3, sort_keys=True)
