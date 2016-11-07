55d29911ee54d0a9262786f31cbf5a7d6cb44b3c
Ben Webb <bjwebb67@googlemail.com>
2016-11-07 17:00:49 +0000

    Update travis tests to validate against 1.0.1 schema

--- validate.sh
+++ validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # exit on error
-wget "http://ocds.open-contracting.org/standard/r/1__0__0/release-package-schema.json"
-wget "http://ocds.open-contracting.org/standard/r/1__0__0/release-schema.json"
+wget "http://ocds.open-contracting.org/standard/r/1__0__1/release-package-schema.json"
+wget "http://ocds.open-contracting.org/standard/r/1__0__1/release-schema.json"
 cd fictional-example
 # Check that files validate
 for f in *.json; do echo $f; python ../jsonschema_with_formatchecker.py ../release-package-schema.json -i  $f; done
