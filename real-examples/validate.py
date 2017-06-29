4da13d25e10799bfa1176b87db928158ca1d37e9
Anna Powell-Smith <annapowellsmith@gmail.com>
2017-06-29 18:22:44 +0100

    Improve validation script: add version options, better error messages.
    - Supply a command line version option
    - Handle the error in the v1.1 release schema
    - Better error messages

--- real-examples/validate.py
+++ real-examples/validate.py
@@ -10,10 +10,12 @@ Validate a set of JSON files against a given OCDS schema.
 '''
 
 
-def get_schema(name):
+def get_schema(name, version):
+    version = '__'.join(version.split('.'))
     url = 'http://standard.open-contracting.org/schema/'
-    url += '1__1__0/%s' % name
+    url += '%s/%s' % (version, name)
     r = requests.get(url)
+    print(url)
     return r.json()
 
 
@@ -22,18 +24,25 @@ def main():
     parser = optparse.OptionParser(usage=usage)
     parser.add_option('-f', '--filepath', action='store', default=None,
                       help='Path to files, e.g. paraguay/sample')
+    parser.add_option('-v', '--version', action='store', default='1.1.0',
+                      help='Version, e.g. 1.1.0')
     parser.add_option(
-        '-t', '--type', action='store', default='release-package',
+        '-t', '--type', action='store', default='release',
         help='File type: release-package, record-package or release')
     (options, args) = parser.parse_args()
     if not options.filepath:
         parser.error('You must supply a filepath, using the -f argument')
-    schema = get_schema('%s-schema.json' % options.type)
+    schema = get_schema('%s-schema.json' % options.type, options.version)
+    if options.type == 'record-package' and options.version == '1.1.0':
+        # Fix v1.1 schema error - wrong item is required.
+        schema['required'].remove('releases')
+        schema['required'].append('records')
     for filename in glob.glob('%s/*.json' % options.filepath):
         if not filename.endswith('.json'):
             print('Skipping non-JSON file %s' % filename)
             continue
         with open(filename, 'r') as file:
+            # print('\n%s' % filename)
             try:
                 data = json.load(file)
             except Exception as e:
@@ -43,8 +52,11 @@ def main():
             try:
                 validate(data, schema)
             except Exception as e:
-                print('Problem validating', filename)
-                data['validationErrors'] = str(e)
+                print('\nProblem validating', filename)
+                location = '/'.join(e.absolute_schema_path)
+                message = e.message
+                print("%s: %s" % (location, message))
+                data['validationErrors'] = str("%s: %s" % (location, message))
             with open(filename, 'w') as writefile:
                 writefile.write(json.dumps(data, indent=2))
 
