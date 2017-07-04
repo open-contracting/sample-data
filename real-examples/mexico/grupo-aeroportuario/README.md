463cd000a97debde296a83ccabb9d24d681a3cea
Anna Powell-Smith <annapowellsmith@gmail.com>
2017-07-04 20:38:49 +0100

    Refactor to use common functions

--- real-examples/mexico/grupo-aeroportuario/README.md
+++ real-examples/mexico/grupo-aeroportuario/README.md
@@ -3,27 +3,6 @@ Mexico > Grupo Aeroportuario de la Ciudad de México
 
 OCDS files for major contracts of the Mexico City Airport project are accessible at https://datos.gob.mx/busca/organization/gacm
 
-The process to obtain a sample:
+There were 496 available releases as of 2017/07/03.
 
-    python3 fetch.py
-
-Or to obtain all releases:
-
-    python3 fetch.py --all
-
-There were 271 available release packages as of 2017/07/03.
-
-This publisher only publishes releases, so you will first need to transform the releases for v1.1 and validate them, then merge the transformed releases into records:
-
-    python3 ../../update_to_v1_1.py -f sample/releases
-    python3 ../../validate.py -f sample/releases
-
-Currently they pass validation.
-
-Then transform the releases into records:
-
-    python ../../merge_releases.py -f sample/releases -o sample/records
-
-Finally you can upload all records to S3:
-
-    aws s3 sync sample/releases s3://ocds1/releases --exclude '.DS_Store' --exclude '.keep'
+Currently these releases pass validation.
--- real-examples/mexico/grupo-aeroportuario/fetch.py
+++ real-examples/mexico/grupo-aeroportuario/fetch.py
@@ -1,47 +1,25 @@
-import json
 import optparse
+import os
 
 import requests
 
-
-def writeFile(fname, data, url):
-    try:
-        with open(fname, 'w', encoding='utf8') as f:
-            f.write(json.dumps(data, indent=2, ensure_ascii=False))
-    except Exception as e:
-        with open("errors.txt", 'a') as err:
-            print("Failed to write %s, %s" % (fname, e))
-            err.write(
-                "Failed to write %s, %s, %s\n" % (fname, e, url))
+from common import common
 
 
-def writeReleases(releases, folder, data, url):
-    for i, r in enumerate(releases):
-        r['packageInfo'] = {
-            'uri': data['uri'],
-            'publishedDate': data['publishedDate'],
-            'publisher': data['publisher']
-        }
-        fname = '%s/releases/%s-%s.json' % \
-            (folder, r['ocid'],
-             r['id'].replace('.', '_').replace(':', '_'))
-        writeFile(fname, r, url)
-        if folder == 'sample' and i >= 10:
-            break
-
-
-def getReleases(url, folder):
-    print('fetching', url)
+def getReleasePackages(url):
+    '''
+    This publisher publishes a list of links to release packages,
+    in a custom format.
+    Retrieve the URLs of all the release packages.
+    '''
     r = requests.get(url)
     data = r.json()
-    if folder == 'sample':
-        for d in data[:10]:
-            writeReleases(d['releases'], folder, d, url)
-    else:
-        if type(data) == list:
-            writeReleases(d['releases'], folder, d, url)
-        else:
-            writeReleases(d['releases'], folder, d, url)
+    urls = []
+    for result in data['result']['results']:
+        for resource in result['resources']:
+            if resource['format'] == 'JSON':
+                urls.append(resource['url'])
+    return urls
 
 
 def main():
@@ -52,20 +30,22 @@ def main():
     (options, args) = parser.parse_args()
     url = 'http://datos.gob.mx/busca/api/3/action/'
     url += 'package_search?q=organization:gacm&rows=500'
-    r = requests.get(url)
-    data = r.json()
-    sources = []
-    for result in data['result']['results']:
-        for resource in result['resources']:
-            if resource['format'] == 'JSON':
-                sources.append(resource['url'])
+    folder = os.path.dirname(os.path.realpath(__file__))
+    release_packages = getReleasePackages(url)
     if options.all:
-        for s in sources:
-            getReleases(s, 'all')
+        folder += '/all'
     else:
-        for s in sources[:1]:
-            getReleases(s, 'sample')
-
+        release_packages = release_packages[:1]
+        folder += '/sample'
+    for url in release_packages:
+        print('fetching %s' % url)
+        data = common.getUrlAndRetry(url, folder)
+        if type(data) == list:
+            for d in data:
+                common.writeReleases(d['releases'], folder, d, url)
+        else:
+            common.writeReleases(
+                data['releases'], folder, data, url)
 
 if __name__ == '__main__':
     main()
