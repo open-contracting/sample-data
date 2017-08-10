6df36381c2b781141c906aeba30cb8669935fb36
Anna Powell-Smith <annapowellsmith@gmail.com>
2017-08-10 10:41:44 +0100

    Update Ukraine scripts to use common functions
    Former-commit-id: f4cd68041219120e003b17b3ed873aeb5ad53c52

--- real-examples/ukraine/fetch.py
+++ real-examples/ukraine/fetch.py
@@ -1,33 +1,10 @@
-import json
 import optparse
-from pprint import pprint
+import os
 
 from pyquery import PyQuery as pq
 import requests
 
-
-def writeFile(fname, data, url):
-    try:
-        with open(fname, 'w', encoding='utf8') as f:
-            f.write(json.dumps(data, indent=2, ensure_ascii=False))
-    except Exception as e:
-        with open("errors.txt", 'a') as err:
-            print("Failed to write %s, %s" % (fname, e))
-            err.write(
-                "Failed to write %s, %s, %s\n" % (fname, e, url))
-
-
-def writeReleases(releases, folder, data, url):
-    for i, r in enumerate(releases):
-        r['packageInfo'] = {
-            'uri': data['uri'],
-            'publishedDate': data['publishedDate'],
-            'publisher': data['publisher']
-        }
-        fname = '%s/releases/%s-%s.json' % (folder, r['ocid'], r['id'])
-        writeFile(fname, r, url)
-        if folder == 'sample' and i >= 10:
-            break
+from common import common
 
 
 def main():
@@ -35,9 +12,22 @@ def main():
     parser = optparse.OptionParser(usage=usage)
     parser.add_option('-a', '--all', action='store_true', default=False,
                       help='Fetch all records, rather than a small extract')
+    parser.add_option(
+        '-R', '--resume', action='store_true', default=False,
+        help='Continue from last page (in page.n) for when download broken')
+    parser.add_option('-p', '--page', action='store', type="int", default=1,
+                      help='Start from page n of the results')
     (options, args) = parser.parse_args()
+    if options.resume:
+        with open("page.n", 'r') as n:
+                page = n.read()
+    else:
+        page = options.page
     BASE = 'http://ocds.prozorro.openprocurement.io'
+    folder = os.path.dirname(os.path.realpath(__file__))
     if options.all:
+        folder += '/all'
+        urls = []
         r = requests.get(BASE)
         d = pq(r.content)
         links = d('.container ol li a')
@@ -48,19 +38,25 @@ def main():
             links = d('.container ol li a')
             for l in links:
                 release_url = "%s/%s" % (latest_url, l.attrib['href'])
-                print('Fetching %s' % release_url)
-                r = requests.get(release_url)
-                data = r.json()
-                writeReleases(data['releases'], 'all', data, release_url)
+                urls.append(release_url)
         else:
             print('Latest URL does not contain extensions - check page')
     else:
-        url = '%s/merged_with_extensions_2017-06-23/release-0000001.json' \
-            % BASE
-        r = requests.get(url)
-        data = r.json()
-        writeReleases(data['releases'], 'sample', data, url)
-
+        folder += '/sample'
+        urls = [
+            '%s/merged_with_extensions_2017-06-23/release-0000001.json' % BASE
+        ]
+    print('%s release packages to fetch' % len(urls))
+    for url in urls:
+        package_num = int(url.split('-')[-1].replace('.json', ''))
+        if package_num < options.page:
+            continue
+        print('fetching %s' % url)
+        data = common.getUrlAndRetry(url, folder)
+        common.writeReleases(
+            data['releases'], folder, data, url)
+        with open("page.n", 'w') as n:
+            n.write(str(package_num))
 
 if __name__ == '__main__':
     main()
