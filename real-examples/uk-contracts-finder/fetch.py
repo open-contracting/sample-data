09c1d9288f267d6377f95a2ccb565028f9dd6813
Anna Powell-Smith <annapowellsmith@gmail.com>
2017-08-10 10:47:25 +0100

    Update UK scripts to use common functions
    Former-commit-id: 00b8353a17cb17d6d26b8420cba30a4cb21e3d1f

--- real-examples/uk-contracts-finder/fetch.py
+++ real-examples/uk-contracts-finder/fetch.py
@@ -1,50 +1,9 @@
 import optparse
+import os
+
 import requests
-import json
 
-
-def writeFile(fname, data, url):
-    try:
-        with open(fname, 'w', encoding='utf8') as f:
-            f.write(json.dumps(data, indent=2, ensure_ascii=False))
-    except Exception as e:
-        with open("errors.txt", 'a') as err:
-            print("Failed to write %s, %s" % (fname, e))
-            err.write(
-                "Failed to write %s, %s, %s\n" % (fname, e, url))
-
-
-def writeReleases(page, folder, maxpage=0):
-    BASE = 'https://www.contractsfinder.service.gov.uk'
-    url = '%s/Published/Notices/OCDS/Search?order=asc&page=%s' % (BASE, page)
-    print("Fetching page %s: %s" % (page, url))
-    r = requests.get(url)
-    data = r.json()
-    try:
-        if maxpage > -1:
-            maxpage = data['maxPage']
-        results = data['results']
-        for i, result in enumerate(results):
-            # TODO: Write records here, as well as results.
-            for j, release in enumerate(results[i]['releases']):
-                fname = folder + '/releases/' + \
-                    result['releases'][j]['id'] + ".json"
-                writeFile(fname, release, url)
-                if folder == 'sample' and i >= 10:
-                    return
-    except:
-        with open("errors.txt", 'a') as err:
-            print("Failed to get " + str(page))
-            err.write("%s\n" % url)
-    # Keep track of which page has been reached.
-    page = int(page) + 1
-    with open("page.n", 'w') as n:
-        n.write(str(page))
-    if page <= maxpage or int(maxpage) == 0:
-        fetchData(page, folder)
-    else:
-        with open("page.n", 'w') as n:
-            n.write("1")
+from common import common
 
 
 def main():
@@ -54,19 +13,44 @@ def main():
                       help='Fetch all records, rather than a small extract')
     parser.add_option(
         '-R', '--resume', action='store_true', default=False,
-        help='Continue from last page (in page.n) for when download broken')
+        help='Continue from the last page (in page.n)')
     parser.add_option('-p', '--page', action='store', type="int", default=1,
                       help='Start from page n of the results')
+
     (options, args) = parser.parse_args()
+
     if options.resume:
         with open("page.n", 'r') as n:
             page = n.read()
     else:
         page = options.page
+    folder = os.path.dirname(os.path.realpath(__file__))
+
+    BASE = 'https://www.contractsfinder.service.gov.uk'
+    url = '%s/Published/Notices/OCDS/Search?order=asc&page=%s' % (BASE, 1)
     if options.all:
-        writeReleases(page, 'all')
+        folder += '/all'
+        r = requests.get(url)
+        data = r.json()
+        num_pages = data['maxPage']
+        print('%s pages to retrieve' % num_pages)
+        for i in range(page, num_pages+1):
+            url = '%s/Published/Notices/OCDS/Search?order=asc&page=%s' % \
+                (BASE, i)
+            print('fetching %s' % url)
+            data = common.getUrlAndRetry(url, folder)
+            for r in data['results']:
+                common.writeReleases(r['releases'], folder, r, url)
+            with open("page.n", 'w') as n:
+                n.write(str(i))
+        with open("page.n", 'w') as n:
+            n.write("1")
     else:
-        writeReleases(page, 'sample', -1)
+        folder += '/sample'
+        print('fetching %s' % url)
+        data = common.getUrlAndRetry(url, folder)
+        for r in data['results'][:10]:
+            common.writeReleases(r['releases'], folder, r, url)
 
 if __name__ == '__main__':
     main()
