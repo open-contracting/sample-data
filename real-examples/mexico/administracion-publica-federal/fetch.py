ee44ce2f8e922ff8555abcbe36cb708f03856792
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2018-01-10 15:28:49 -0300

    Mexico APF data
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
    
    Former-commit-id: 81671e55d21b815a4a20c2ed7c18fd14af858f8e

--- real-examples/mexico/administracion-publica-federal/fetch.py
+++ real-examples/mexico/administracion-publica-federal/fetch.py
@@ -51,12 +51,12 @@ def main():
         folder += '/sample'
     for url in release_packages:
         print('fetching %s' % url)
-   #     urllib.request.urlretrieve(url, "file.zip")
-    #    with zipfile.ZipFile("file.zip", "r") as zip_ref:
-     #       zip_ref.extractall(os.getcwd())
+        urllib.request.urlretrieve(url, "file.zip")
+        with zipfile.ZipFile("file.zip", "r") as zip_ref:
+            zip_ref.extractall(os.getcwd())
+            zip_ref.close()
         with open(os.getcwd()+'/contratacionesabiertas_bulk.json') as data_file:
             data = json.load(data_file, encoding='latin1')
-            print (data[0])
             if not options.all:
                 data = data[:10]
             if type(data) == list:
@@ -66,7 +66,8 @@ def main():
             else:
                 common.writeReleases(
                     data['releases'], folder, data, url)
-
+            data_file.close()
+        break
 
 if __name__ == '__main__':
     main()
