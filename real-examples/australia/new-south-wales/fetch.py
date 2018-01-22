d9c6e5c0b60fe011bf30e044dc8687ad256128de
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2018-01-22 08:33:52 -0300

    Modify the australia script
    Add the option -b for fetch the data in bigquery format (default false)
    and download and save the full release package without modifications by
    default for validate it through the validator.
    
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- real-examples/australia/new-south-wales/fetch.py
+++ real-examples/australia/new-south-wales/fetch.py
@@ -9,10 +9,13 @@ def main():
     parser = optparse.OptionParser(usage=usage)
     parser.add_option('-a', '--all', action='store_true', default=False,
                       help='Fetch all records, rather than a small extract')
+    parser.add_option('-b', '--bigquery', action='store_true', default=False,
+                      help='Fetch records in bigquery format')
     (options, args) = parser.parse_args()
     url = 'https://tenders.nsw.gov.au'
-    url += '/?event=public.api.%s.search&ResultsPerPage=1000'
+    url += '/?event=public.api.%s.search&ResultsPerPage=50'
     folder = os.path.dirname(os.path.realpath(__file__))
+    count = 0
     if options.all:
         folder += '/all'
         release_types = ['planning', 'tender', 'contract']
@@ -21,19 +24,26 @@ def main():
             while next_url:
                 print('fetching', next_url)
                 data = common.getUrlAndRetry(next_url, folder)
-                common.writeReleases(
-                    data['releases'], folder, data, next_url)
+                if options.bigquery:
+                    common.writeReleases(
+                        data['releases'], folder, data, next_url)
+                else:
+                    common.writeFile('%s.json' % count, folder, data, next_url)
                 if 'next' in data['links']:
                     next_url = data['links']['next']
                 else:
                     next_url = None
+                count = count + 1
     else:
         folder += '/sample'
         next_url = url % 'planning'
         print('fetching', next_url)
         data = common.getUrlAndRetry(next_url, folder)
-        common.writeReleases(
-            data['releases'], folder, data, next_url)
+        if options.bigquery:
+            common.writeReleases(
+                data['releases'], folder, data, next_url)
+        else:
+            common.writeFile('%s.json' % count, folder, data, next_url)
 
 
 if __name__ == '__main__':
