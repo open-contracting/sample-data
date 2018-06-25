24c542dc3ab0686c2737ab54aa44edeb8834e254
James McKinney <james@slashpoundbang.com>
2018-06-25 13:51:52 -0400

    Use 2-space indentation for JSON files
    Former-commit-id: c66f9fa29090f230b47fdc2a796b7fd89b3e2a5f

--- fictional-example/1.0/merge.py
+++ fictional-example/1.0/merge.py
@@ -36,7 +36,8 @@ with open("record/ocds-213czf-000-00001.json", 'w') as f:
             'releases': releases,
             'compiledRelease': compiled_release,
         }]
-    }, f, indent=3, sort_keys=True)
+    }, f, indent=2, sort_keys=True)
+    f.write('\n')
 
 with open("record/ocds-213czf-000-00001-withversions.json", 'w') as f:
     json.dump({
@@ -50,4 +51,5 @@ with open("record/ocds-213czf-000-00001-withversions.json", 'w') as f:
             'compiledRelease': compiled_release,
             'versionedRelease': versioned_release,
         }]
-    }, f, indent=3, sort_keys=True)
+    }, f, indent=2, sort_keys=True)
+    f.write('\n')
