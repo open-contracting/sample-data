11ff9367b7509582e56c3ce3a9d86469eb76ee2a
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-10-28 17:57:48 -0400

    test: Move parameter-building into a function

--- tests/test_fictional_example.py
+++ tests/test_fictional_example.py
@@ -8,20 +8,6 @@ import requests
 from jsonschema import FormatChecker
 from jsonschema.validators import Draft4Validator as validator
 
-versions = {
-    '1.0': '1__0__3',
-    '1.1': '1__1__5',
-}
-
-url_template = 'https://standard.open-contracting.org/schema/{}/release-package-schema.json'
-
-test_valid_argvalues = []
-for minor_version, patch_version in versions.items():
-    schema = requests.get(url_template.format(patch_version)).json()
-    filenames = glob(os.path.join('fictional-example', minor_version, '*.json'))
-    assert len(filenames), '{} fixtures not found'.format(minor_version)
-    test_valid_argvalues += [(filename, schema) for filename in filenames]
-
 
 def custom_warning_formatter(message, category, filename, lineno, line=None):
     return str(message).replace(os.getcwd() + os.sep, '')
@@ -30,7 +16,26 @@ def custom_warning_formatter(message, category, filename, lineno, line=None):
 warnings.formatwarning = custom_warning_formatter
 
 
-@pytest.mark.parametrize('filename,schema', test_valid_argvalues)
+def get_test_cases():
+    test_valid_argvalues = []
+
+    versions = {
+        '1.0': '1__0__3',
+        '1.1': '1__1__5',
+    }
+
+    url_template = 'https://standard.open-contracting.org/schema/{}/release-package-schema.json'
+
+    for minor_version, patch_version in versions.items():
+        schema = requests.get(url_template.format(patch_version)).json()
+        filenames = glob(os.path.join('fictional-example', minor_version, '*.json'))
+        assert len(filenames), '{} fixtures not found'.format(minor_version)
+        test_valid_argvalues += [(filename, schema) for filename in filenames]
+
+    return test_valid_argvalues
+
+
+@pytest.mark.parametrize('filename,schema', get_test_cases())
 def test_valid(filename, schema):
     errors = 0
 
