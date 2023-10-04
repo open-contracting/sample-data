7123849c8104dc023a290aab9fe19ad23b8032cf
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-10-03 21:37:21 -0400

    test: Use referencing package

--- tests/test_fictional_example.py
+++ tests/test_fictional_example.py
@@ -7,6 +7,7 @@ import pytest
 import requests
 from jsonschema import FormatChecker
 from jsonschema.validators import Draft4Validator as validator
+from referencing import Registry, Resource
 
 
 def custom_warning_formatter(message, category, filename, lineno, line=None):
@@ -27,22 +28,29 @@ def get_test_cases():
     url_template = 'https://standard.open-contracting.org/schema/{}/release-package-schema.json'
 
     for minor_version, patch_version in versions.items():
-        schema = requests.get(url_template.format(patch_version)).json()
+        package_schema_url = url_template.format(patch_version)
+        package_schema = requests.get(package_schema_url).json()
+
+        release_schema_url = package_schema_url.replace('-package', '')
+        release_schema = requests.get(release_schema_url).json()
+
+        registry = Registry().with_resource(release_schema_url, Resource.from_contents(release_schema))
+
         filenames = glob(os.path.join('fictional-example', minor_version, '*.json'))
         assert len(filenames), f'{minor_version} fixtures not found'
-        test_valid_argvalues += [(filename, schema) for filename in filenames]
+        test_valid_argvalues += [(filename, registry, package_schema) for filename in filenames]
 
     return test_valid_argvalues
 
 
-@pytest.mark.parametrize('filename,schema', get_test_cases())
-def test_valid(filename, schema):
+@pytest.mark.parametrize('filename,registry,schema', get_test_cases())
+def test_valid(filename, registry, schema):
     errors = 0
 
     with open(filename) as f:
         data = json.load(f)
 
-    for error in validator(schema, format_checker=FormatChecker()).iter_errors(data):
+    for error in validator(schema, format_checker=FormatChecker(), registry=registry).iter_errors(data):
         errors += 1
         warnings.warn(json.dumps(error.instance, indent=2))
         warnings.warn(f"{error.message} ({'/'.join(error.absolute_schema_path)})\n")
